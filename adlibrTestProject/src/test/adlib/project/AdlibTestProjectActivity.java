5604225b5d352bc6bacd20a9f1f819be996039cb
berrybana <hana@mocoplex.com>
2012-11-18 17:27:40 +0900

    to adlibr 3.0

--- adlibrTestProject/src/test/adlib/project/AdlibTestProjectActivity.java
+++ adlibrTestProject/src/test/adlib/project/AdlibTestProjectActivity.java
@@ -62,10 +62,10 @@ public class AdlibTestProjectActivity extends AdlibActivity {
         this.findViewById(R.id.btn3).setOnClickListener(cl);        
     }
     
+    // AndroidManifest.xml에 권한과 activity를 추가하여야 합니다.     
     protected void initAds()
     {
-    	// 광고 스케줄링 설정을 위해 아래 내용을 프로그램 실행시 한번만 실행합니다. (처음 실행되는 activity에서 한번만 호출해주세요.)
-    	
+    	// 광고 스케줄링 설정을 위해 아래 내용을 프로그램 실행시 한번만 실행합니다. (처음 실행되는 activity에서 한번만 호출해주세요.)    	
     	// 광고 subview 의 패키지 경로를 설정합니다. (실제로 작성된 패키지 경로로 수정해주세요.)
     	
     	// 제휴 플랫폼을 연결합니다.
@@ -81,11 +81,13 @@ public class AdlibTestProjectActivity extends AdlibActivity {
         AdlibConfig.getInstance().bindPlatform("TAD","test.adlib.project.ads.SubAdlibAdViewTAD");
         AdlibConfig.getInstance().bindPlatform("NAVER","test.adlib.project.ads.SubAdlibAdViewNaverAdPost");
         AdlibConfig.getInstance().bindPlatform("SHALLWEAD","test.adlib.project.ads.SubAdlibAdViewShallWeAd");
-        // 쓰지 않을 플랫폼은 JAR 파일 및 test.adlib.project.ads 경로에서 삭제하면 최종 바이너리 크기를 줄일 수 있습니다.
+        // 쓰지 않을 플랫폼은 JAR 파일 및 test.adlib.project.ads 경로에서 삭제하면 최종 바이너리 크기를 줄일 수 있습니다.        
         
+        // SMART* dialog 노출 시점 선택시 / setAdlibKey 키가 호출되는 activity 가 시작 activity 이며 해당 activity가 종료되면 app 종료로 인식합니다.
         // adlibr.com 에서 발급받은 api 키를 입력합니다.
         // http://adlibr.com/admin/myapplist.jsp
-        AdlibConfig.getInstance().setAdlibKey("ADLIB - API - KEY");
+        AdlibConfig.getInstance().setAdlibKey("ADLIB - API - KEY");        
+                
 
          /*
          // Locale 별 다른 스케줄을 적용하신다면,
@@ -104,9 +106,14 @@ public class AdlibTestProjectActivity extends AdlibActivity {
          }
          */
 
-	/*
-	// 광고뷰가 없는 activity 에서는 listener 대신 아래와 같은 방법으로 설정한 현재 버전을 가져올 수 있습니다.
-	// 애드립에서 설정한 버전정보를 아래와 같이 수신합니다.
+        
+        /* 
+         * deprecated : SMART* dialog 를 통해 보다 쉽게 버전관리를 할 수 있습니다.
+         */
+
+		/*
+		// 광고뷰가 없는 activity 에서는 listener 대신 아래와 같은 방법으로 설정한 현재 버전을 가져올 수 있습니다.
+		// 애드립에서 설정한 버전정보를 아래와 같이 수신합니다.
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
           @Override
@@ -114,8 +121,8 @@ public class AdlibTestProjectActivity extends AdlibActivity {
        	  	String ver = _amanager.getCurrentVersion();
           }
         }, 1000);
-	*/
-
+	 	*/        
+        /*
         // 클라이언트 버전관리를 위한 리스너 추가
         // 서버에서 클라이언트 버전을 관리하여 사용자에게 업데이트를 안내할 수 있습니다. (option)
         this.setVersionCheckingListner(new AdlibVersionCheckingListener(){
@@ -148,8 +155,8 @@ public class AdlibTestProjectActivity extends AdlibActivity {
 			    .show();
 				
 			}
-        	
         });
+        */
         
     }
 }
